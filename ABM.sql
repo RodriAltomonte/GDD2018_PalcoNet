f1a6713b872a9a75dd4957286bbefcf035698d5d
juanmacan <kanogonzafer@hotmail.com>
2018-11-02 17:18:35 -0300

    ABM
    Agregue nuevas ABM faltan algunas y habria que verificar lo que hice

--- ABM.sql
+++ ABM.sql
@@ -1,6 +1,9 @@
+
+---¡REVISAR TODOS ANTES DE ENVIAR!--------
+---LAS BAJAS SACAR LO DE DELETE SOLO CAMBIAR EL HABILITADO
 ----------------------ABM ROL--------------------
 ----------------------ALTA-----------------------
-CREATE PROCEDURE LOS_DE_GESTION.RolNuevo @RolNombre NVARCHAR(255)
+CREATE PROCEDURE LOS_DE_GESTION.RolNuevo @RolNombre NVARCHAR(255),@habilitado BIT
 AS
 	BEGIN
 		DECLARE @result AS NUMERIC(12)
@@ -8,8 +11,8 @@ AS
 				BEGIN TRY
 					IF(NOT EXISTS(SELECT nombre FROM LOS_DE_GESTION.Rol WHERE nombre=@RolNombre))
 						BEGIN
-							INSERT INTO LOS_DE_GESTION.Rol(nombre)
-							VALUES (@RolNombre)
+							INSERT INTO LOS_DE_GESTION.Rol(nombre,habilitado)
+							VALUES (@RolNombre,@habilitado)
 							SET @result =1
 							END
 					ELSE
@@ -34,12 +37,19 @@ AS
 	DECLARE @result NUMERIC(12)
 	BEGIN TRAN funcs
 		BEGIN TRY 
+		IF(@FuncionalidadRol NOT EXISTS(SELECT id_Funcionalidad FROM LOS_DE_GESTION.Funcionalidad_X_Rol WHERE id_Funcionalidad =@FuncionalidadRol))
+		BEGIN
 		INSERT INTO LOS_DE_GESTION.Rol_X_Funcionalidad(id_Rol,id_Funcionalidad)
 		VALUES ((
 					SELECT id_Rol
 					FROM LOS_DE_GESTION.Rol
 					WHERE nombre=@RolNombre),@FuncionalidadRol)
 					SET @result = 1
+		END
+		ELSE
+		BEGIN
+		SET @result =0
+		END
 					SELECT @result AS resultado
 					COMMIT TRAN funcs
 		END TRY
@@ -53,12 +63,26 @@ AS
 GO
 
 -------------------------------------------BAJA---------------------------------------------
---FALTA HACER ESTE NO SABIA HACERLO
---CREATE PROCEDURE LOS_DE_GESTION.BorrarRol @id_Rol NUMERIC(18,0)
---AS
---BEGIN
-
-
+CREATE PROCEDURE LOS_DE_GESTION.BorrarRol @id_Rol NUMERIC(18,0)
+AS
+BEGIN
+DECLARE @result NUMERIC(12)
+BEGIN TRAN delrol
+BEGIN TRY
+UPDATE LOS_DE_GESTION.Rol
+SET habilitado=0
+WHERE id_Rol =@id_Rol
+SET @result=1
+SELECT @result as resultado
+COMMIT TRAN delrol
+END TRY
+BEGIN CATCH
+ROLLBACK TRAN delrol
+SET @result = 0
+SELECT @result AS resultado
+END CATCH
+END
+GO
 
 CREATE PROCEDURE LOS_DE_GESTION.BorrarFuncionalidades @RolNombre NVARCHAR(255)
 AS
@@ -67,7 +91,7 @@ DECLARE @result NUMERIC(12)
 BEGIN TRAN delfuncs
 BEGIN TRY
 DELETE FROM LOS_DE_GESTION.Rol_X_Funcionalidad
-WHERE id_Rol = (SELECT id_Rol FROM LOS_DE_GESTION.Rol WHERE nombre = @RolNombre)
+WHERE id_Rol = @RolNombre
 SET @result = 1
 SELECT @result AS resultado
 COMMIT TRAN delfuncs
@@ -112,3 +136,333 @@ SELECT @result AS resultado
 END CATCH
 END
 GO
+
+CREATE PROCEDURE ModificarFuncionalidad @id_funcionalidad NUMERIC(18,0)
+AS
+-------------------------------ABM USUARIOS----------------------------------------
+------------------------------ALTA CLIENTE------------------------------------------------
+CREATE PROCEDURE LOS_DE_GESTION.NuevoCliente
+@username NVARCHAR(255),
+@password NVARCHAR(255),
+@rol NVARCHAR(255),
+@nombre NVARCHAR(255),
+@apellido NVARCHAR(255),
+@tipo_documento NVARCHAR(255),
+@nro_documento NUMERIC(18,0),
+@cuil NVARCHAR(255),
+@mail NVARCHAR(255),
+@telefono NVARCHAR(50),
+@direccion_calle NVARCHAR(255),
+@numero_piso NUMERIC(18,0),
+@departamento NVARCHAR(255),
+@localidad NVARCHAR(255),
+@codigo_postal NVARCHAR(255),
+@fecha_de_nacimiento DATETIME,
+@fecha_de_creacion DATETIME,
+@tarjeta NVARCHAR(255)
+
+AS
+BEGIN
+DECLARE @result NUMERIC(12)
+DECLARE @fecha_nacimiento DATETIME
+DECLARE @fecha_creac DATETIME
+SET @fecha_nacimiento = CONVERT(DATETIME,@fecha_de_nacimiento,121)
+SET @fecha_de_creac = CONVERT(DATETIME,@fecha_de_creacion,121)
+BEGIN TRAN nuclie
+
+BEGIN TRY 
+		IF(@nro_documento NOT IN (SELECT numero_documento FROM LOS_DE_GESTION.Cliente WHERE username = @username)
+			AND @cuil NOT IN (SELECT cuil FROM LOS_DE_GESTION.Cliente WHERE @username=username))
+			BEGIN 
+				INSERT INTO LOS_DE_GESTION.Cliente(username,nombre,apellido,tipo_documento,numero_documento,
+				cuil,mail,telefono,calle,nro_piso,depto,localidad,codigo_postal,fecha_nacimiento,fecha_creaacion,tarjeta)
+				VALUES (@username,@nombre,@apellido,@tipo_documento,@nro_documento,@cuil,@mail,@telefono,
+				@direccion_calle,@numero_piso,@departamento,@localidad,@codigo_postal,@fecha_nacimiento,@fecha_creac,@tarjeta)
+
+
+				INSERT INTO LOS_DE_GESTION.Usuario(username,password,id_Rol)
+				VALUES (@username,@password,(SELECT id_Rol FROM LOS_DE_GESTION.Rol WHERE nombre =@rol))
+				SET @result=1
+			END
+
+			ELSE
+			BEGIN 
+			SET @result = 0
+			END
+SELECT @result AS resultado
+COMMIT TRAN nuclie
+END TRY
+
+BEGIN CATCH
+ROLLBACK TRAN nuclie
+SET @result = 2
+SELECT @result AS resultado
+END CATCH
+
+END
+GO
+
+
+
+-----------------------ALTA EMPRESA-------------
+CREATE PROCEDURE LOS_DE_GESTION.NuevaEmpresa
+@username NVARCHAR(255),
+@pasword NVARCHAR(255),
+@rol NVARCHAR(255),
+@razon_social NVARCHAR(255),
+@mail NVARCHAR(50),
+@telefono NUMERIC(18,0),
+@direccion_calle NUMERIC(18,0),
+@codigo_postal NVARCHAR(50),
+@ciudad NVARCHAR(255),
+@ciut NVARCHAR(255)
+
+AS 
+BEGIN
+DECLARE @result NUMERIC(12)
+BEGIN TRAN nuemp
+BEGIN TRY
+IF(@username NOT IN (SELECT username FROM LOS_DE_GESTION.Usuario WHERE username = @username)) --MODIFICAR ESTO
+BEGIN 
+INSERT INTO LOS_DE_GESTION.Empresa(username,razon_social,cuit,mail,telefono,calle,codigo_postal,ciudad)
+VALUES (@username,@razon_social,@ciut,@mail,@telefono,@direccion_calle,@codigo_postal,@ciudad)
+
+INSERT INTO LOS_DE_GESTION.Usuario(username,password,id_Rol)
+VALUES(@username,@pasword,(SELECT id_rol FROM LOS_DE_GESTION.Rol WHERE nombre = @rol))
+
+
+SET @result = 1
+END
+ELSE 
+BEGIN
+SET @result =0
+END
+SELECT @result AS resultado
+COMMIT TRAN nuemp
+END TRY
+
+BEGIN CATCH
+ROLLBACK TRAN nuemp
+SET @result = 2
+SELECT @result AS resultado
+END CATCH
+END
+GO
+
+
+
+---------------------------BAJA CLIENTE--------------------
+CREATE PROCEDURE LOS_DE_GESTION.BorrarCliente
+@nombre NVARCHAR(255)=NULL,
+@apellido NVARCHAR(255)=NULL,
+@dni NVARCHAR(255)=NULL,
+@email NVARCHAR(255)=NULL
+AS
+BEGIN
+DECLARE @result NUMERIC(12)
+BEGIN TRAN delclie
+BEGIN TRY
+DELETE FROM LOS_DE_GESTION.Cliente
+WHERE nombre=@nombre OR @nombre IS NULL AND apellido = @apellido OR @apellido IS NULL
+AND SUBSTRING(cuil,3,10) = @dni OR @dni IS NULL
+
+--DELETE FROM LOS_DE_GESTION.Usuario
+
+update LOS_DE_GESTION.Usuario
+set habilitado = 0
+WHERE username = (SELECT username FROM LOS_DE_GESTION.Cliente WHERE nombre = @nombre)
+SET @result =1
+SELECT @result AS resultado
+COMMIT TRAN delclie
+END TRY
+
+BEGIN CATCH
+ROLLBACK TRAN delclie
+SET @result = 0
+SELECT @result AS resultado
+END CATCH
+END
+GO
+
+
+
+-------------------------BAJA EMPRESA--------------------------
+CREATE PROCEDURE LOS_DE_GESTION.BorrarEmpresa
+@razon_social NVARCHAR(255)=NULL,
+@cuit NVARCHAR(255)=NULL,
+@email NVARCHAR(50)=NULL
+
+AS 
+BEGIN
+DECLARE @result NUMERIC(12)
+BEGIN TRAN delemp
+BEGIN TRY
+DELETE FROM LOS_DE_GESTION.Empresa
+WHERE razon_social = @razon_social OR @razon_social IS NULL
+AND cuit = @cuit OR @cuit IS NULL
+AND mail = @email OR @email IS NULL
+
+DELETE FROM LOS_DE_GESTION.Usuario
+WHERE username = (SELECT username FROM LOS_DE_GESTION.Empresa WHERE razon_social = @razon_social)
+SET @result = 1
+SELECT @result AS resultado
+COMMIT TRAN delemp
+END TRY
+
+BEGIN CATCH
+ROLLBACK TRAN delemp
+SET @result = 2
+SELECT @result AS resultado
+END CATCH
+
+END
+GO
+
+
+--------------------MODIFICACION CLIENTE----------------------------
+CREATE PROCEDURE LOS_DE_GESTION.ModificarEstadoCliente @username NVARCHAR(255)
+AS
+BEGIN
+DECLARE @result NUMERIC(12)
+BEGIN TRAN modestado
+BEGIN TRY 
+IF((SELECT habilitado FROM LOS_DE_GESTION.Cliente WHERE username = @username)=0)
+BEGIN 
+UPDATE LOS_DE_GESTION.Cliente
+SET habilitado = 1
+WHERE username = @username;
+SET @result = 0
+END
+
+ELSE
+BEGIN 
+UPDATE LOS_DE_GESTION.Cliente
+SET habilitado=0
+WHERE username = @username
+SET @result =1 
+END
+SELECT @result AS resultado
+COMMIT TRAN modestado
+END TRY
+BEGIN CATCH
+ROLLBACK TRAN modestado
+SET @result =2
+SELECT @result AS resultado
+END CATCH
+END
+GO
+
+CREATE PROCEDURE LOS_DE_GESTION.ModificarPasswordCliente @username NVARCHAR(255),@passwordNuevo NVARCHAR(255)
+AS
+BEGIN
+DECLARE @result NUMERIC(12)
+BEGIN TRAN modpass
+BEGIN TRY 
+UPDATE LOS_DE_GESTION.Usuario
+SET password = @passwordNuevo
+WHERE username = @username
+
+SET @result = 1
+SELECT @result AS resultado
+COMMIT TRAN modpass
+END TRY
+BEGIN CATCH
+ROLLBACK TRAN modpass
+SET @result= 0
+SELECT @result AS resultado
+END CATCH
+END
+GO
+
+
+CREATE PROCEDURE LOS_DE_GESTION.ModificarCliente
+@nombre NVARCHAR(255)=NULL,
+@apellido NVARCHAR(255)=NULL,
+@dni NVARCHAR(255)=NULL,
+@email NVARCHAR(255)=NULL,
+@password NVARCHAR(255),
+@tipo_documento NVARCHAR(255),
+@nro_documento NUMERIC(18,0),
+@cuil NVARCHAR(255),
+@mail NVARCHAR(255),
+@telefono NVARCHAR(50),
+@dir_calle NVARCHAR(255),
+@nro_piso NUMERIC(18,0),
+@depto NVARCHAR(255),
+@localidad NVARCHAR(255),
+@codigo_postal NVARCHAR(255),
+@fecha_nacimiento DATETIME
+@fecha_de_creacion DATETIME
+
+AS
+BEGIN
+DECLARE @result NUMERIC(12)
+BEGIN TRAN modclie
+BEGIN TRY
+UPDATE LOS_DE_GESTION.CLiente 
+SET nombre =@nombre,
+	apellido = @apellido,
+	cuil =@cuil,
+	mail = @email,
+
+COMMIT TRAN modclie
+END TRY
+
+BEGIN CATCH
+ROLLBACK TRAN modclie
+SET @result = 1
+SELECT @result AS resultado
+END CATCH
+END
+GO
+
+
+-------------------MODIFICACION EMPRESA------------------------
+--TODO
+CREATE PROCEDURE LOS_DE_GESTION.ModificarPasswordEmpresa @username NVARCHAR(255),@passwordNuevo NVARCHAR(255)
+
+
+CREATE PROCEDURE LOS_DE_GESTION.ModificarEstadoEmpresa
+@razon_social NVARCHAR(255)=NULL,
+@cuit NVARCHAR(255)=NULL,
+@email NVARCHAR(255)=NULL,
+@habilitado BIT
+
+AS
+BEGIN
+DECLARE @result NUMERIC(12)
+BEGIN TRAN modemp
+BEGIN TRY
+
+
+
+
+
+COMMIT TRAN modemp
+END TRY
+
+BEGIN CATCH
+ROLLBACK TRAN modemp
+
+END CATCH
+END
+GO
+
+
+-----------------------ABM PUBLICACION--------------------
+-----------------------ALTA------------------------------
+CREATE PROCEDURE LOS_DE_GESTION.NuevaPublicacion
+
+
+
+
+
+
+
+---------------------BAJA----------------------------------
+CREATE PROCEDURE LOS_DE_GESTION.BorrarPublicacion
+
+
+
+--------------------MODIFICACION----------------------------
+CREATE PROCEDURE LOS_DE_GESTION.ModificarPublicacion
