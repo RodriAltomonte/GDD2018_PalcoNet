61e5efd372840760ccd99a955e0960c7a14f211b
Ronan Camargo <ronan.cam@gmail.com>
2018-11-04 18:21:10 -0300

    Agrego pantalla para el listado estadistico

--- PalcoNet/ListadoEstadistico/ListadoEstadisticoForm.Designer.cs
+++ PalcoNet/ListadoEstadistico/ListadoEstadisticoForm.Designer.cs
@@ -28,11 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpBusqueda = new System.Windows.Forms.GroupBox();
+            this.lblTipoListado = new System.Windows.Forms.Label();
+            this.lblAnio = new System.Windows.Forms.Label();
+            this.lblTrimestre = new System.Windows.Forms.Label();
+            this.cmbTipoListado = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.cmbTrimestre = new System.Windows.Forms.ComboBox();
+            this.txtAnio = new System.Windows.Forms.TextBox();
+            this.grpResultados = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.grpBusqueda.SuspendLayout();
+            this.grpResultados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpBusqueda
+            // 
+            this.grpBusqueda.Controls.Add(this.txtAnio);
+            this.grpBusqueda.Controls.Add(this.cmbTrimestre);
+            this.grpBusqueda.Controls.Add(this.btnBuscar);
+            this.grpBusqueda.Controls.Add(this.cmbTipoListado);
+            this.grpBusqueda.Controls.Add(this.lblTrimestre);
+            this.grpBusqueda.Controls.Add(this.lblAnio);
+            this.grpBusqueda.Controls.Add(this.lblTipoListado);
+            this.grpBusqueda.Location = new System.Drawing.Point(12, 24);
+            this.grpBusqueda.Name = "grpBusqueda";
+            this.grpBusqueda.Size = new System.Drawing.Size(504, 140);
+            this.grpBusqueda.TabIndex = 0;
+            this.grpBusqueda.TabStop = false;
+            this.grpBusqueda.Text = "Filtros de Búsqueda";
+            // 
+            // lblTipoListado
+            // 
+            this.lblTipoListado.AutoSize = true;
+            this.lblTipoListado.Location = new System.Drawing.Point(36, 36);
+            this.lblTipoListado.Name = "lblTipoListado";
+            this.lblTipoListado.Size = new System.Drawing.Size(76, 13);
+            this.lblTipoListado.TabIndex = 0;
+            this.lblTipoListado.Text = "Tipo de listado";
+            // 
+            // lblAnio
+            // 
+            this.lblAnio.AutoSize = true;
+            this.lblAnio.Location = new System.Drawing.Point(36, 65);
+            this.lblAnio.Name = "lblAnio";
+            this.lblAnio.Size = new System.Drawing.Size(26, 13);
+            this.lblAnio.TabIndex = 1;
+            this.lblAnio.Text = "Año";
+            // 
+            // lblTrimestre
+            // 
+            this.lblTrimestre.AutoSize = true;
+            this.lblTrimestre.Location = new System.Drawing.Point(250, 65);
+            this.lblTrimestre.Name = "lblTrimestre";
+            this.lblTrimestre.Size = new System.Drawing.Size(50, 13);
+            this.lblTrimestre.TabIndex = 2;
+            this.lblTrimestre.Text = "Trimestre";
+            // 
+            // cmbTipoListado
+            // 
+            this.cmbTipoListado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipoListado.FormattingEnabled = true;
+            this.cmbTipoListado.Location = new System.Drawing.Point(128, 33);
+            this.cmbTipoListado.Name = "cmbTipoListado";
+            this.cmbTipoListado.Size = new System.Drawing.Size(339, 21);
+            this.cmbTipoListado.TabIndex = 3;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(392, 100);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // cmbTrimestre
+            // 
+            this.cmbTrimestre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTrimestre.FormattingEnabled = true;
+            this.cmbTrimestre.Location = new System.Drawing.Point(306, 62);
+            this.cmbTrimestre.Name = "cmbTrimestre";
+            this.cmbTrimestre.Size = new System.Drawing.Size(161, 21);
+            this.cmbTrimestre.TabIndex = 5;
+            // 
+            // txtAnio
+            // 
+            this.txtAnio.Location = new System.Drawing.Point(128, 62);
+            this.txtAnio.Name = "txtAnio";
+            this.txtAnio.Size = new System.Drawing.Size(100, 20);
+            this.txtAnio.TabIndex = 6;
+            // 
+            // grpResultados
+            // 
+            this.grpResultados.Controls.Add(this.dataGridView1);
+            this.grpResultados.Location = new System.Drawing.Point(13, 181);
+            this.grpResultados.Name = "grpResultados";
+            this.grpResultados.Size = new System.Drawing.Size(503, 228);
+            this.grpResultados.TabIndex = 1;
+            this.grpResultados.TabStop = false;
+            this.grpResultados.Text = "Resultados";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(491, 168);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ListadoEstadisticoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 429);
+            this.Controls.Add(this.grpResultados);
+            this.Controls.Add(this.grpBusqueda);
+            this.Name = "ListadoEstadisticoForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ListadoEstadisticoForm_Load);
+            this.grpBusqueda.ResumeLayout(false);
+            this.grpBusqueda.PerformLayout();
+            this.grpResultados.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpBusqueda;
+        private System.Windows.Forms.TextBox txtAnio;
+        private System.Windows.Forms.ComboBox cmbTrimestre;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.ComboBox cmbTipoListado;
+        private System.Windows.Forms.Label lblTrimestre;
+        private System.Windows.Forms.Label lblAnio;
+        private System.Windows.Forms.Label lblTipoListado;
+        private System.Windows.Forms.GroupBox grpResultados;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- PalcoNet/ListadoEstadistico/ListadoEstadisticoForm.cs
+++ PalcoNet/ListadoEstadistico/ListadoEstadisticoForm.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace PalcoNet.ListadoEstadistico
 {
     public partial class ListadoEstadisticoForm : Form
     {
+        private const string EMPRESAS_LOCALIDADES_NO_VENDIDAS = "Empresa con mayor cantidad de localidades no vendidas";
+        private const string CLIENTES_MAS_PUNTOS_VENCIDOS = "Clientes con mayores puntos vencidos";
+        private const string CLIENTES_MAS_COMPRAS = "Clientes con mayor cantidad de compras";
+
+        private const string PRIMER_TRIMESTRE = "Enero - Abril";
+        private const string SEGUNDO_TRIMESTRE = "Mayo - Agosto";
+        private const string TERCER_TRIMESTRE = "Septiembre - Diciembre";
+
         public ListadoEstadisticoForm()
         {
             InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (ValidarFiltros())
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Los filtros ingresados son inválidos.");
+            }
+        }
+
+        private void ListadoEstadisticoForm_Load(object sender, EventArgs e)
+        {
+            cmbTipoListado.Items.Add(EMPRESAS_LOCALIDADES_NO_VENDIDAS);
+            cmbTipoListado.Items.Add(CLIENTES_MAS_COMPRAS);
+            cmbTipoListado.Items.Add(CLIENTES_MAS_COMPRAS);
+
+            cmbTrimestre.Items.Add(PRIMER_TRIMESTRE);
+            cmbTrimestre.Items.Add(SEGUNDO_TRIMESTRE);
+            cmbTrimestre.Items.Add(TERCER_TRIMESTRE);
+        }
+
+        private Boolean ValidarFiltros()
+        {
+            Regex regexNumeros = new Regex("^[0-9]*$");
+            return regexNumeros.IsMatch(txtAnio.Text) && cmbTipoListado.SelectedItem != null && cmbTrimestre.SelectedItem != null;
         }
     }
 }
